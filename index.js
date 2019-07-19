7711d9e41163ab0ba90184818624f65407ee12ac
namchee <cristophernamchee12@gmail.com>
2019-07-19 10:56:40 +0700

    test

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { router } from './src/routes/routes';
-import line from '@line/bot-sdk';
 
 const app = express();
 const port = process.env.PORT || 3000;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,7 +7,7 @@ import { handleEvents } from './../eventhandler';
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
-}
+};
 
 const client = new Client(config);
 
